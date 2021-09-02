71bc77db0240eff94d583ecf66d8da65f204dbc3
aritonkaramani <ariton.karamani@gmail.com>
2021-09-02 22:35:44 +0200

    productscreen fixed, need to add mobile friendly

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -3,7 +3,7 @@ import './productScreen.scss';
 
 export default function ProductScreen() {
     return (
-        <div className="product">
+        <div className="products">
             <div className="left">
                 <div className="leftImg">
                     <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="" />
